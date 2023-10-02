762e89acb631dc43c0028a75ac679ff8c433dcd0
gnarlsley <thompson.garrett7413@gmail.com>
2023-10-01 22:51:02 -0500

    Pushed to git after reading instruction

--- MovieListGarrettThompson/Program.cs
+++ MovieListGarrettThompson/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}"); //addition of slug
 
 app.Run();
